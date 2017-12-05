da1bb68602513a214c4b007f1bb00c3a7df828ce
chrisbradshaw <christopherbradshaw11@gmail.com>
2017-12-05 14:50:40 -0500

    update urls.py

--- fitmarketapp/urls.py
+++ fitmarketapp/urls.py
@@ -4,4 +4,13 @@ from fitmarketapp import views
 urlpatterns = [
     url(r'^$', views.home, name='home'),
     url(r'^gigs/(?P<id>[0-9]+)/$', views.gig_detail, name='gig_detail'),
+    url(r'^my_gigs/$', views.my_gigs, name='my_gigs'),
+    url(r'^create_gig/$', views.create_gig, name='create_gig'),
+    url(r'^edit_gig/(?P<id>[0-9]+)/$', views.edit_gig, name='edit_gig'),
+    url(r'^profile/(?P<username>\w+)/$', views.profile, name='profile'),
+    url(r'^checkout/$', views.create_purchase, name='create_purchase'),
+    url(r'^my_sellings/$', views.my_sellings, name='my_sellings'),
+    url(r'^my_buyings/$', views.my_buyings, name='my_buyings'),
+    url(r'^category/(?P<link>[\w|-]+)/$', views.category, name='category'),
+    url(r'^search/$', views.search, name='search'),
 ]
