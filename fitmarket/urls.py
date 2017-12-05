31fe88f7460e98024dfecb4ae7df9f7a71f4ccb0
chrisbradshaw <christopherbradshaw11@gmail.com>
2017-12-05 15:43:35 -0500

    update urls.py

--- fitmarket/urls.py
+++ fitmarket/urls.py
@@ -16,9 +16,12 @@ Including another URLconf
 from django.conf.urls import url, include
 from django.contrib import admin
 
+from django.conf import settings
+from django.conf.urls.static import static
+
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url('^social/', include('social.apps.django_app.urls', namespace='social')),
     url('^auth/', include('django.contrib.auth.urls', namespace='auth')),
     url('', include('fitmarketapp.urls'))
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
