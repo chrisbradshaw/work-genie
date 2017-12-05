9fb4eea612102dd705654d2baff251840a878751
chrisbradshaw <christopherbradshaw11@gmail.com>
2017-12-05 14:58:01 -0500

    add forms, add social auth

--- fitmarketapp/admin.py
+++ fitmarketapp/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Profile, Gig, Purchase, Review
 
 # Register your models here.
+admin.site.register(Profile)
+admin.site.register(Gig)
+admin.site.register(Purchase)
+admin.site.register(Review)
